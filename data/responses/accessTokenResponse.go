fe157b762387dc595a7f836caee5e779c17485e8
mofe <ogunbiyioladapo33@gmail.com>
2023-07-23 22:04:23 +0100

    updated dtos

--- data/responses/accessTokenResponse.go
+++ data/responses/accessTokenResponse.go
@@ -1,6 +1,7 @@
 package responses
 
 type AccessTokenResponse struct {
+	StatusCode       int    `json:"status_code,omitempty"`
 	AccessToken      string `json:"access_token,omitempty"`
 	TokenType        string `json:"token_type,omitempty"`
 	ExpiresIn        int16  `json:"expires_in,omitempty"`
--- data/responses/apiResponse.go
+++ data/responses/apiResponse.go
@@ -4,6 +4,7 @@ import "time"
 
 type APIResponse struct {
 	Status    int                    `json:"status"`
+	Success   bool                   `json:"success"`
 	Message   string                 `json:"message"`
 	Timestamp time.Time              `json:"timestamp"`
 	Data      map[string]interface{} `json:"data"`
