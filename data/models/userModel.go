c5b735210564ff5a2d2247657462a0aa5003ac31
mofe <ogunbiyioladapo33@gmail.com>
2023-07-26 14:05:57 +0100

    updated user model

--- data/models/userModel.go
+++ data/models/userModel.go
@@ -1,6 +1,10 @@
 package models
 
+import "go.mongodb.org/mongo-driver/bson/primitive"
+
 type User struct {
-	Email    string
-	Password string
+	Id       primitive.ObjectID `json:"id" bson:"_id"`
+	Username string             `json:"username" binding:"required"`
+	Email    string             `json:"email" binding:"required,email"`
+	Password string             `json:"password" binding:"required"`
 }
