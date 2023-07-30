08e8c8cac7c255a416cd55515731296eda801396
mofe <ogunbiyioladapo33@gmail.com>
2023-07-30 12:42:25 +0100

    updated dto

---
+++ data/responses/spotifyUserProfile.go
@@ -0,0 +1,9 @@
+package responses
+
+type SpotifyUserProfile struct {
+	Country     string `json:"country"`
+	DisplayName string `json:"display_name"`
+	Email       string `json:"email"`
+	Id          string `json:"id"`
+	Product     string `json:"product"`
+}
