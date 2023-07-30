c11396564d533894793fec60ceabe9caad71ea34
mofe <ogunbiyioladapo33@gmail.com>
2023-07-30 12:40:45 +0100

    updated db config

--- config/db.go
+++ config/db.go
@@ -16,7 +16,7 @@ func ConnectDB() *mongo.Client {
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
 	// Create Mongo Client and connect
-	client, err := mongo.NewClient(options.Client().ApplyURI(EnvMongoURI()))
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI(EnvMongoURI()))
 	if err != nil {
 		util.ErrorLog.Fatalln("Error connecting to Mongodb", err)
 	}
