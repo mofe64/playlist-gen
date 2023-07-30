bffb5b8a2b00ae39e7ba40e3cba1c92c715af272
mofe <ogunbiyioladapo33@gmail.com>
2023-07-30 12:42:03 +0100

    token model impl

---
+++ data/models/token.go
@@ -0,0 +1,8 @@
+package models
+
+type Token struct {
+	AccessToken  string `json:"access_token"`
+	TokenType    string `json:"token_type"`
+	Scope        string `json:"scope"`
+	RefreshToken string `json:"refresh_token"`
+}
