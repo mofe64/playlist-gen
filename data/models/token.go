e531533b043ee1ab0ea182a268251fbd307e4905
mofe <ogunbiyioladapo33@gmail.com>
2023-07-31 00:47:45 +0100

    updated token model

--- data/models/token.go
+++ data/models/token.go
@@ -1,8 +1,12 @@
 package models
 
+import "time"
+
 type Token struct {
-	AccessToken  string `json:"access_token"`
-	TokenType    string `json:"token_type"`
-	Scope        string `json:"scope"`
-	RefreshToken string `json:"refresh_token"`
+	AccessToken  string    `json:"access_token"`
+	TokenType    string    `json:"token_type"`
+	Scope        string    `json:"scope"`
+	RefreshToken string    `json:"refresh_token"`
+	ExpiresIn    int16     `json:"expires_in,omitempty"`
+	IssuedAt     time.Time `json:"issued_at"`
 }
