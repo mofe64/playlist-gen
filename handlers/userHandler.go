57cc5698ad160de5bee8ef11d0815a509a8459a6
mofe <ogunbiyioladapo33@gmail.com>
2023-08-01 15:39:53 +0100

    updated user handler

--- handlers/userHandler.go
+++ handlers/userHandler.go
@@ -10,32 +10,78 @@ import (
 	"time"
 
 	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/bson"
 )
 
 func CreatePlaylist() gin.HandlerFunc {
 	tag := "CREATE_PLAYLIST_HANDLER"
 	return func(c *gin.Context) {
-		_, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+		ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 		defer cancel()
 		userId := c.Param("userId")
 		value := c.GetString("userDetails")
-		var tokenDetails models.Token
+		util.InfoLog.Println(tag+" : userdetails on req ctx", value)
+		var sessionDetails models.Session
 		if len(value) == 0 {
-			// retrieve token details from db
-		} else {
-			err := json.Unmarshal([]byte(value), &tokenDetails)
+			// initialize session details to saved user auth
+			var user models.User
+			err := userCollection.FindOne(ctx, bson.M{"id": userId}).Decode(&user)
 			if err != nil {
-				util.ErrorLog.Println(tag+": could not parse auth token", value)
+				c.JSON(
+					http.StatusBadRequest,
+					responses.APIResponse{
+						Status:    http.StatusNotFound,
+						Message:   "Not found",
+						Timestamp: time.Now(),
+						Data:      gin.H{"error": "No user found with Id " + userId},
+						Success:   false,
+					},
+				)
+				return
+			}
+			sessionDetails = user.Auth
+		} else {
+			err := json.Unmarshal([]byte(value), &sessionDetails)
+			if err != nil {
+				util.ErrorLog.Println(tag+": could not parse session details", err.Error())
+				c.JSON(
+					http.StatusInternalServerError,
+					responses.APIResponse{
+						Status:    http.StatusInternalServerError,
+						Message:   "Something went wrong",
+						Timestamp: time.Now(),
+						Data:      gin.H{"error": "Internal Error, please try again"},
+						Success:   false,
+					},
+				)
+				return
 			}
 		}
+		util.InfoLog.Println(tag+": parsed session details ", sessionDetails)
+		userTopTracks, err := spotifyService.GetUserTopTracks(sessionDetails.AccessToken)
+		if err != nil {
+			util.ErrorLog.Println(tag+": could not retrieve user top tracks", err.Error())
+			c.JSON(
+				http.StatusInternalServerError,
+				responses.APIResponse{
+					Status:    http.StatusInternalServerError,
+					Message:   "Something went wrong",
+					Timestamp: time.Now(),
+					Data:      gin.H{"error": "Internal Error, please try again"},
+					Success:   false,
+				},
+			)
+			return
+		}
 
-		util.InfoLog.Println(tag+": user Id", userId)
 		c.JSON(http.StatusOK, responses.APIResponse{
 			Status:    http.StatusOK,
 			Message:   "Success",
 			Timestamp: time.Now(),
-			Data:      gin.H{},
-			Success:   true,
+			Data: gin.H{
+				"topTracks": userTopTracks,
+			},
+			Success: true,
 		})
 	}
 }
