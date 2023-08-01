3621b4b6ca40d8e68becd8a920af0cb7e5706b44
mofe <ogunbiyioladapo33@gmail.com>
2023-08-01 15:37:44 +0100

    refactores user model to use session

--- data/models/userModel.go
+++ data/models/userModel.go
@@ -1,10 +1,10 @@
 package models
 
 type User struct {
-	Id          string `json:"id"`
-	Username    string `json:"username"`
-	Email       string `json:"email"`
-	Country     string `json:"country"`
-	SpotifyPlan string `json:"spotify_plan"`
-	Auth        Token  `json:"auth"`
+	Id          string  `json:"id"`
+	Username    string  `json:"username"`
+	Email       string  `json:"email"`
+	Country     string  `json:"country"`
+	SpotifyPlan string  `json:"spotify_plan"`
+	Auth        Session `json:"auth"`
 }
